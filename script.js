67a38a73cb428833bffa8b49ae287105d4ebb8f9
Luke Swan <lukeswan23@gmail.com>
2018-09-07 17:30:40 +1000

    refactored JS code & added random gradient background button

--- script.js
+++ script.js
@@ -2,15 +2,36 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var random = document.querySelector(".random");
 
-body.style.background
+//display initial CSS properties
+colorChange()
+
+
+
+function randomGradient() {
+    console.log("random button");
+    var Rcolor1 = "#" + Math.floor(Math.random()*16777215).toString(16);
+    var Rcolor2 = "#" + Math.floor(Math.random()*16777215).toString(16);
+    // console.log(Rcolor1);
+    // console.log(Rcolor2);
+    body.style.background = "linear-gradient(to right, " + Rcolor1 + ", " + Rcolor2 + ")";
+    css.textContent = body.style.background + ";";
+
+}
 
 function colorChange (){
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
     css.textContent = body.style.background + ";";
+    // console.log(color1.value)
+    // console.log(color2.value)
 }
+
+
 
 
 color1.addEventListener("input", colorChange);
 
 color2.addEventListener("input", colorChange);
+
+random.addEventListener("click", randomGradient);
